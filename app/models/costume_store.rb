a6f059bb0bc3035d134cd4d839e422c54eb94348
Sharon Watkins <swatkins876@gmail.com>
2019-11-18 13:35:22 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,10 +1,10 @@
 class CostumeStore < ActiveRecord::Base
 
-  attr_accessor :name, :location, :costume_inventory, :employee_count, :still_in_business, :opening_time, :closing_time 
+  attr_accessor :name, :location, :costume_inventory, :employee_count, :still_in_business, :opening_time, :closing_time
 
   def initialize(name, location, costume_inventory, employee_count, still_in_business, opening_time, closing_time)
     @name = name
-    @location = location 
+    @location = location
     @costume_inventory = costume_inventory
     @employee_count = employee_count
     @still_in_business = still_in_business
