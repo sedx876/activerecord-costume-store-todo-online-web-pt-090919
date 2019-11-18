e073813819627a76fe1d9af22008d3f8b1b37d24
Sharon Watkins <swatkins876@gmail.com>
2019-11-18 13:35:19 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,3 +1,15 @@
 class CostumeStore < ActiveRecord::Base
+
+  attr_accessor :name, :location, :costume_inventory, :employee_count, :still_in_business, :opening_time, :closing_time 
+
+  def initialize(name, location, costume_inventory, employee_count, still_in_business, opening_time, closing_time)
+    @name = name
+    @location = location 
+    @costume_inventory = costume_inventory
+    @employee_count = employee_count
+    @still_in_business = still_in_business
+    @opening_time = opening_time
+    @closing_time = closing_time
+  end
 end
 # Create your CostumeStore class here
