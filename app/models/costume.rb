dbf2df191f0f70b1da1583cd8fe57fa9c43d1d9b
Sharon Watkins <swatkins876@gmail.com>
2019-11-18 13:24:42 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,4 +1,9 @@
 class Costume < ActiveRecord::Base
+  attr_accessor :image_url
+
+  def intialize(image_url:,)
+    @image_url = image_url 
+  end
 end
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
